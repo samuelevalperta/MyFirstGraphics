9e1795a4e03fec54e9cf8220c17c0d4108b264e8
Samuele Valperta <samuele.valperta@gmail.com>
2023-10-19 15:54:15 +0200

    Add MouseMotionListener, enhance graphic representation in PanelGraphics
    PanelGraphics.java now implements MouseMotionListener to detect mouse movements. Created a constructor that adds a MouseMotionListener. Updated the method paintComponent, alternating red and blue circles based on their positions for visual enhancement. Incorporated tracking and visualization of mouse movement: the number of movements and the current position are now displayed on the screen. The color of the moving circle is yellow.

--- src/it/unibs/pajc/PanelGraphics.java
+++ src/it/unibs/pajc/PanelGraphics.java
@@ -6,8 +6,12 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
-public class PanelGraphics extends JPanel {
+public class PanelGraphics extends JPanel implements MouseMotionListener {
     private JPanel panel1;
+
+    public PanelGraphics() {
+        this.addMouseMotionListener(this);
+    }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -26,11 +30,24 @@ public class PanelGraphics extends JPanel {
         int d = 60;
         int r = d/2;
 
+        boolean red;
         for (int xc = r; xc <= (w+r); xc += 2 * r) {
-            for (int yc = r; xc <= (h+r); yc += 2 * r) {
-                //fillCircle(g2, xc, yc, r);
+
+            // this will make the first circle of every column to be different from the first of the previous column
+            red = (xc / (2 * r) % 2 == 0 ? true : false);
+
+            for (int yc = r; yc <= (h+r); yc += 2 * r) {
+                g2.setColor(red ? Color.red : Color.blue);
+                fillCircle(g2, xc, yc, r);
+                red = !red;
             }
         }
+
+        g2.setColor(Color.black);
+        g2.drawString(String.format("[%3d]", mouse_moved_counter),10, 10);
+
+        g2.setColor(Color.yellow);
+        fillCircle(g2, mouse_current_x, mouse_current_y, r);
 
     }
 
@@ -39,5 +56,21 @@ public class PanelGraphics extends JPanel {
         int y = (yc - r);
         int d = r * 2;
         g2.fillOval(x, y, d, d);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    private int mouse_moved_counter;
+    private int mouse_current_x;
+    private int mouse_current_y;
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        System.out.printf("Mouse Moved[%d]: %d, %d\n",mouse_moved_counter++, e.getX(), e.getY());
+        mouse_current_x = e.getX();
+        mouse_current_y = e.getY();
+        this.repaint();
     }
 }
